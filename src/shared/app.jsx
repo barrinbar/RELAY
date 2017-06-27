4e713d413d60850abf8edb1570120d7677c911ec
barrinbar <barrinbar@gmail.com>
2017-06-27 09:46:48 +0300

    sync

--- src/shared/app.jsx
+++ src/shared/app.jsx
@@ -5,7 +5,7 @@ import { Switch } from 'react-router'
 import { Route } from 'react-router-dom'
 import Helmet from 'react-helmet'
 
-import RulesPage from './component/page/rules'
+// import RulesPage from './component/page/rules'
 import HelloAsyncPage from './component/page/hello-async'
 import HomePage from './component/page/home'
 import Footer from './component/footer'
@@ -14,7 +14,7 @@ import NotFoundPage from './component/page/not-found'
 import { APP_NAME } from './config'
 import {
   HOME_PAGE_ROUTE,
-  RULES_PAGE_ROUTE,
+  // RULES_PAGE_ROUTE,
   HELLO_ASYNC_PAGE_ROUTE,
 } from './routes'
 
@@ -24,7 +24,6 @@ const App = () =>
     <Nav />
     <Switch>
       <Route exact path={HOME_PAGE_ROUTE} render={() => <HomePage />} />
-      <Route path={RULES_PAGE_ROUTE} render={() => <RulesPage />} />
       <Route path={HELLO_ASYNC_PAGE_ROUTE} render={() => <HelloAsyncPage />} />
       <Route component={NotFoundPage} />
     </Switch>
